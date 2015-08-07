498e0d7df515810bf06df998ffed6e16e516cf56
ZZB <realbo89@outlook.com>
2015-08-07 11:32:05 +0800

    属性动画shake

--- library/src/main/java/com/zzb/library/utils/AnimUtils.java
+++ library/src/main/java/com/zzb/library/utils/AnimUtils.java
@@ -1,5 +1,6 @@
 package com.zzb.library.utils;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.view.View;
 import android.view.animation.Animation;
@@ -23,5 +24,15 @@ public class AnimUtils {
         view.clearAnimation();
         Animation shake = AnimationUtils.loadAnimation(context, R.anim.shake);
         view.startAnimation(shake);
+    }
+    /**
+     * 左右摆View
+     *@author ZZB
+     *created at 2015/8/7 11:16
+     */
+    public static void shake(View view){
+        float tLeft = -10;
+        float tRight = 10;
+        ObjectAnimator.ofFloat(view, "translationX", 0, tRight, tLeft, tRight, tLeft, tRight, tLeft, tRight, tLeft, 0).setDuration(800).start();
     }
 }
